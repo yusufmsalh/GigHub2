4b58e158ee762c9fcc695534e123ee211706a927
yusufmsalh <yusufm.salh@gmail.com>
2018-11-17 10:19:11 +0200

    Adding New Gigs Form with ddl fix

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GigHub.Models;
+using GigHub.ViewModels;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext dbContext;
+
+        public GigsController()
+        {
+            dbContext = new ApplicationDbContext();
+
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+           GigFormViewModel gigsViewModel = new GigFormViewModel()
+            {
+                Generes = dbContext.Generes.ToList()
+            };
+            return View(gigsViewModel);
+        }
+    }
+}
---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using GigHub.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genere { get; set; }
+        public IEnumerable<Genere> Generes { get; set; }//List of Geners from DB
+
+    }
+}
