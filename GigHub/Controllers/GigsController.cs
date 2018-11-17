a71c6aa15ab171c3ee48f267f4a5e798c32d4b49
yusufmsalh <yusufm.salh@gmail.com>
2018-11-17 10:51:32 +0200

    Adding Create Gig Post

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GigHub.Migrations;
 using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace GigHub.Controllers
 {
+    [Authorize]
     public class GigsController : Controller
     {
         private ApplicationDbContext dbContext;
@@ -18,6 +21,8 @@ namespace GigHub.Controllers
 
         }
         // GET: Gigs
+      
+        [HttpGet]
         public ActionResult Create()
         {
            GigFormViewModel gigsViewModel = new GigFormViewModel()
@@ -26,5 +31,20 @@ namespace GigHub.Controllers
             };
             return View(gigsViewModel);
         }
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artist = dbContext.Users.Single(u => u.Id == User.Identity.GetUserId());// application user object that can associated with gig
+          var gig = new Gig()
+          {
+              Artist = artist,
+              DateTime = DateTime.Parse(string.Format("{0} {1}" ,viewModel.Date,viewModel.Time)),
+
+          }
+
+            return View();
+        }
+
+
     }
 }
