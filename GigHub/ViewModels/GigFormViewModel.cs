d06409fc7518efba2fce89a03f3f4fcc404dc46c
yusufmsalh <yusufm.salh@gmail.com>
2019-01-24 19:24:36 +0200

    Replacing Magic(Fixed) String with Expression<Func> to get method name in run time.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,10 +1,13 @@
-﻿using GigHub.Migrations;
+﻿using GigHub.Controllers;
+using GigHub.Migrations;
 using GigHub.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
+using System.Web.Mvc;
 
 namespace GigHub.ViewModels
 {
@@ -34,7 +37,37 @@ namespace GigHub.ViewModels
         public string Heading { get; set; }// use it to to change the heading of view ,as it's shared among multiple actions
         public string ActionToBeCalled
         {
-            get { return (Id != 0) ? "Edit" : "Create"; }
+            #region Getting Action Name in Run Time
+
+            get
+            {
+                Expression<Func<GigsController, ActionResult>> UpdateMethod = (c => c.Edit(this));
+                Expression<Func<GigsController, ActionResult>> CreateMethod = (c => c.Create(this));
+                var actionToBeCalled = (Id != 0) ? UpdateMethod : CreateMethod;
+                return (actionToBeCalled.Body as MethodCallExpression).Method.Name;
+            }
+
+            #region Above Part Explantion
+            /* use the above part to get the method name at run time
+             * to replace get { return (Id != 0) ? "Edit" : "Create"; }
+             *and avoid magic(static) strings ,as if method name changed this
+             * will break your code
+             * Func : Pointer to a method
+             * input of func : Gigs controller
+             * return of func : action result from edit / create method  
+             * this : a dummy value as edit method expects a viewmodel
+             * func allows as to call that method (c => c.Edit(this))
+             * but we don't want to call ,we want to save it as an expression
+             * finally cast expression to method call expression
+             *use MethodCallExpression to access method Name
+             */
+            //
+            //// avoid magic strings as they are fixed and it method name changed ,
+            /// your code will break down
+
+            #endregion
+            #endregion
+
         }
     }
 }
