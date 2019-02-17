91691e5b6b6d47882c4a4f2719f143f0821f48d4
yusufmsalh <yusufm.salh@gmail.com>
2019-02-17 14:38:33 +0200

    using Animate.css to Animate the Notification Count

--- GigHub/App_Start/BundleConfig.cs
+++ GigHub/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace GigHub
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
+             
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/bootbox.js",
@@ -26,7 +26,9 @@ namespace GigHub
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/Site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/Animate.css"
+                ));
         }
     }
 }
